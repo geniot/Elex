69809652e89f0a0e94efc644a57551aa90097697
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2023-04-09 15:47:11 +0200

    Zip resource files in packages of 100 files

--- src/main/java/io/github/geniot/elex/tools/Sound2Repository.java
+++ src/main/java/io/github/geniot/elex/tools/Sound2Repository.java
@@ -4,13 +4,17 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Sound2Repository {
 
@@ -20,68 +24,88 @@ public class Sound2Repository {
     public static void main(String[] args) {
         try {
             String pathToWavFiles = "C:\\Temp\\SoundEn\\SoundEn.extracted";
-            String pathToRepository = "C:\\dictionaries\\SoundEn";
+            String pathToMp3Files = "C:\\Temp\\SoundEn\\SoundEn.converted";
+            String pathToRepository = "C:\\dictionaries\\SoundEn\\data";
 
+            System.out.println("Converting");
+
+            FileUtils.forceMkdir(new File(pathToMp3Files));
+            wav2mp3(pathToWavFiles, pathToMp3Files);
+            executor.shutdown();
+
+            System.out.println("Zipping");
+
+            File[] files = new File(pathToMp3Files).listFiles();
+
+            int outCounter = 0;
+
+            List<File> batch = new ArrayList<>();
+            for (File f : files) {
+                batch.add(f);
+                if (batch.size() == 100) {
+                    System.out.println(outCounter);
+                    zip(pathToRepository, outCounter, batch);
+                    batch = new ArrayList<>();
+                    ++outCounter;
+                }
+            }
+            if (batch.size() > 0) {
+                zip(pathToRepository, outCounter, batch);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void zip(String pathToRepository, int fileCounter, List<File> files) throws Exception {
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(pathToRepository + File.separator + fileCounter + ".zip"));
+        for (File file : files) {
+            ZipEntry e = new ZipEntry(file.getName());
+            out.putNextEntry(e);
+            byte[] data = Files.readAllBytes(file.toPath());
+            out.write(data, 0, data.length);
+            out.closeEntry();
+        }
+        out.close();
+    }
+
+    public static void wav2mp3(String pathToWavFiles, String pathToMp3Files) {
+        try {
             int counter = 0;
-            int tenThousand = 0;
-            int thousand = 0;
-            int hundred = 0;
 
             File[] files = new File(pathToWavFiles).listFiles();
             List<Future<Integer>> futures = new ArrayList<>();
 
             for (File f : files) {
-
-                StringBuilder path = new StringBuilder();
-                path.append(pathToRepository);
-                path.append(File.separator);
-                path.append("data");
-                path.append(File.separator);
-                path.append(tenThousand);
-                path.append(File.separator);
-                path.append(thousand);
-                path.append(File.separator);
-                path.append(hundred);
-                FileUtils.forceMkdir(new File(path.toString()));
+                String outFile = pathToMp3Files + File.separator + f.getName();
 
                 String[] lameCommand = new String[]{
                         "lame/lame.exe",
                         "-V4",
-                        f.getAbsolutePath(),
-                        path + File.separator + f.getName()
+                        pathToWavFiles + File.separator + f.getName(),
+                        outFile
                 };
 
                 Future<Integer> future = convert(lameCommand);
                 futures.add(future);
+            }
 
+            for (Future<Integer> future : futures) {
+                future.get();
                 ++counter;
-                if (counter % 100 == 0) {
-                    hundred += 1;
-                    if (counter % 10000 == 0) {
-                        tenThousand += 1;
-                    }
-                    if (counter % 1000 == 0) {
-                        thousand += 1;
-                    }
+                if (counter % 1000 == 0) {
+                    System.out.println(counter + "/" + files.length);
                 }
             }
-
-            counter = 0;
-            for (Future<Integer> future : futures) {
-                ++counter;
-                System.out.println(counter);
-                future.get();
-            }
-            executor.shutdown();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static Future<Integer> convert(String[] lameCommand) {
         return executor.submit(() -> {
             try {
-                System.out.println(String.join(" ", lameCommand));
                 ProcessBuilder pb = new ProcessBuilder(Arrays.asList(lameCommand));
                 pb.redirectErrorStream(true);
                 Process process = pb.start();
