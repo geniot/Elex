ca86d313b34ffea1a67deae89fa6770a689bfd4a
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2023-04-09 12:47:47 +0200

    Add sound to rep converter, deprecate wav to mp3

--- src/main/java/io/github/geniot/elex/tools/Sound2Repository.java
+++ src/main/java/io/github/geniot/elex/tools/Sound2Repository.java
@@ -1,7 +1,6 @@
 package io.github.geniot.elex.tools;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.io.FileUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,58 +12,76 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class Wav2Mp3 {
-    static Logger logger = LoggerFactory.getLogger(Wav2Mp3.class);
+public class Sound2Repository {
 
     static int THREADS = 10;
     private static ExecutorService executor = Executors.newFixedThreadPool(THREADS);
 
     public static void main(String[] args) {
         try {
-            String base = "D:\\torrents\\elex\\LingvoUniversalRuEn\\SoundRu.extracted\\";
-            String out = "D:\\torrents\\elex\\LingvoUniversalRuEn\\SoundRu.converted\\";
-
-            new File(out).mkdirs();
+            String pathToWavFiles = "C:\\Temp\\SoundEn\\SoundEn.extracted";
+            String pathToRepository = "C:\\dictionaries\\SoundEn";
 
             int counter = 0;
+            int tenThousand = 0;
+            int thousand = 0;
+            int hundred = 0;
 
-            File[] files = new File(base).listFiles();
+            File[] files = new File(pathToWavFiles).listFiles();
             List<Future<Integer>> futures = new ArrayList<>();
 
             for (File f : files) {
 
-                ++counter;
-                if (counter % 100 == 0) {
-                    System.out.println(counter + "/" + files.length);
-                }
-
-                String outFile = out + File.separator + f.getName();
-                if (new File(outFile).exists()) {
-                    continue;
-                }
+                StringBuilder path = new StringBuilder();
+                path.append(pathToRepository);
+                path.append(File.separator);
+                path.append("data");
+                path.append(File.separator);
+                path.append(tenThousand);
+                path.append(File.separator);
+                path.append(thousand);
+                path.append(File.separator);
+                path.append(hundred);
+                FileUtils.forceMkdir(new File(path.toString()));
 
                 String[] lameCommand = new String[]{
                         "lame/lame.exe",
                         "-V4",
-                        base + File.separator + f.getName(),
-                        outFile
+                        f.getAbsolutePath(),
+                        path + File.separator + f.getName()
                 };
 
                 Future<Integer> future = convert(lameCommand);
                 futures.add(future);
+
+                ++counter;
+                if (counter % 100 == 0) {
+                    hundred += 1;
+                    if (counter % 10000 == 0) {
+                        tenThousand += 1;
+                    }
+                    if (counter % 1000 == 0) {
+                        thousand += 1;
+                    }
+                }
             }
 
+            counter = 0;
             for (Future<Integer> future : futures) {
+                ++counter;
+                System.out.println(counter);
                 future.get();
             }
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
+            executor.shutdown();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
     public static Future<Integer> convert(String[] lameCommand) {
         return executor.submit(() -> {
             try {
+                System.out.println(String.join(" ", lameCommand));
                 ProcessBuilder pb = new ProcessBuilder(Arrays.asList(lameCommand));
                 pb.redirectErrorStream(true);
                 Process process = pb.start();
